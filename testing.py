fe1b8be40e6d21e65e6ae8d878975f3b005ae818
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-04-04 12:53:27 -0500

    testing update

--- testing.py
+++ testing.py
@@ -1,4 +1,3 @@
 testDict = {"test key3" : "test valye3", "test key": "test value", "test key2": "test value2"}
 sort = sorted(testDict.items())
-x = "testing change"
 end = input("Press enter to exit")
