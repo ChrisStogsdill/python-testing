1ff1799b612564e378e2bb420549e90a5fb21d0e
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-04-06 16:23:20 -0500

    download and extract data out of json

---
+++ geo-coding.py
@@ -0,0 +1,14 @@
+# create a geocoding application that searches for a location 
+# and returns the latitude and longitude
+from textwrap import indent
+import urllib.request, urllib.parse, urllib.error
+import json
+
+baseUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip=73130&appid=90b04ea300e9c1626525544025aafc02'
+
+zipData = urllib.request.urlopen(baseUrl).read().decode()
+convertedData = json.loads(zipData)
+print(convertedData['zip'])
+
+
+
